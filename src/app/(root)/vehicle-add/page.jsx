0a98be8d7b065c6644383ca06982987d9928f526
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-10 02:12:22 +0500

    build issues

--- src/app/(root)/vehicle-add/page.jsx
+++ src/app/(root)/vehicle-add/page.jsx
@@ -1,32 +1,32 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
-import { useGetVehicleByIdQuery } from '@/services/vehicle-manage';
-import AddVehicle from '@/modules/AddVehicle';
-import { LoadingOverlay } from '@mantine/core';
-import { Suspense } from 'react';
+// 'use client';
+// import { useSearchParams } from 'next/navigation';
+// import { useGetVehicleByIdQuery } from '@/services/vehicle-manage';
+// import AddVehicle from '@/modules/AddVehicle';
+// import { LoadingOverlay } from '@mantine/core';
+// import { Suspense } from 'react';
 
-export default function EditVehiclePage() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get('id');
-  console.log("id>>>>>>>>>", id)
-  // Only fetch if we have an ID
-  const { data: vehicleData, isLoading } = useGetVehicleByIdQuery(id, {
-    skip: !id // Skip the query if there's no ID
-  });
+// export default function EditVehiclePage() {
+//   const searchParams = useSearchParams();
+//   const id = searchParams.get('id');
+//   console.log("id>>>>>>>>>", id)
+//   // Only fetch if we have an ID
+//   const { data: vehicleData, isLoading } = useGetVehicleByIdQuery(id, {
+//     skip: !id // Skip the query if there's no ID
+//   });
 
-  if (isLoading) {
-    return <LoadingOverlay visible />;
-  }
+//   if (isLoading) {
+//     return <LoadingOverlay visible />;
+//   }
 
  
 
-  // If we have data, render form with edit data
-  return (
-    <Suspense fallback={<LoadingOverlay visible />}>
-      { !id ? <AddVehicle /> : <AddVehicle editData={vehicleData} /> }  
-    </Suspense>
-  );
+//   // If we have data, render form with edit data
+//   return (
+//     <Suspense fallback={<LoadingOverlay visible />}>
+//       { !id ? <AddVehicle /> : <AddVehicle editData={vehicleData} /> }  
+//     </Suspense>
+//   );
 
 
 
-}
+// }

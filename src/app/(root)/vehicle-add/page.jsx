fb10d1b5730177e37ddf94e62327ef0799d98547
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-10 04:38:15 +0500

    compare moduleqa

--- src/app/(root)/vehicle-add/page.jsx
+++ src/app/(root)/vehicle-add/page.jsx
@@ -1,32 +1,32 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
-import { useGetVehicleByIdQuery } from '@/services/vehicle-manage';
-import AddVehicle from '@/modules/addVehicle';
-import { LoadingOverlay } from '@mantine/core';
-import ClientWrapper from '@/components/ClientWrapper';
+// 'use client';
+// import { useSearchParams } from 'next/navigation';
+// import { useGetVehicleByIdQuery } from '@/services/vehicle-manage';
+// import AddVehicle from '@/modules/addVehicle';
+// import { LoadingOverlay } from '@mantine/core';
+// import ClientWrapper from '@/components/ClientWrapper';
 
-export default function EditVehiclePage() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get('id');
-  console.log("id>>>>>>>>>", id)
-  // Only fetch if we have an ID
-  const { data: vehicleData, isLoading } = useGetVehicleByIdQuery(id, {
-    skip: !id // Skip the query if there's no ID
-  });
+// export default function EditVehiclePage() {
+//   const searchParams = useSearchParams();
+//   const id = searchParams.get('id');
+//   console.log("id>>>>>>>>>", id)
+//   // Only fetch if we have an ID
+//   const { data: vehicleData, isLoading } = useGetVehicleByIdQuery(id, {
+//     skip: !id // Skip the query if there's no ID
+//   });
 
-  if (isLoading) {
-    return <LoadingOverlay visible />;
-  }
+//   if (isLoading) {
+//     return <LoadingOverlay visible />;
+//   }
 
  
 
-  // If we have data, render form with edit data
-  return (
-    <ClientWrapper>
-      { !id ? <AddVehicle /> : <AddVehicle editData={vehicleData} /> }  
-    </ClientWrapper>
-  );
+//   // If we have data, render form with edit data
+//   return (
+//     <ClientWrapper>
+//       { !id ? <AddVehicle /> : <AddVehicle editData={vehicleData} /> }  
+//     </ClientWrapper>
+//   );
 
 
 
-}
+// }
--- src/modules/Dashboard/LocationBaseUser/index.jsx
+++ src/modules/Dashboard/LocationBaseUser/index.jsx
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
-import { Box } from '@mantine/core';
-import useLocationBaseUsers from './useLocationBaseUsers';
-import dynamic from 'next/dynamic';
-import styles from './LocationBaseUser.module.css';
-import Card from '@/components/Card';
-const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+// import React, { useState } from 'react';
+// import { Box } from '@mantine/core';
+// import useLocationBaseUsers from './useLocationBaseUsers';
+// import dynamic from 'next/dynamic';
+// import styles from './LocationBaseUser.module.css';
+// import Card from '@/components/Card';
+// const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-export default function LocationBaseUser() {
-  const {
-    series,
-    options
-  } = useLocationBaseUsers();
+// export default function LocationBaseUser() {
+//   const {
+//     series,
+//     options
+//   } = useLocationBaseUsers();
 
-  return (
-    <Box className={styles.section}>
-      <Card noContentPadding>
-        <Box className={styles.cardHeader}>
-          <Box className={styles.cardTitle}>Location Base User</Box>
-        </Box>
+//   return (
+//     <Box className={styles.section}>
+//       <Card noContentPadding>
+//         <Box className={styles.cardHeader}>
+//           <Box className={styles.cardTitle}>Location Base User</Box>
+//         </Box>
 
-        <Box>
-          <Chart
-            options={options}
-            series={series}
-            type="bar"
-            height={350}
-          />
-        </Box>
-      </Card>
-    </Box>
-  )
-}
+//         <Box>
+//           <Chart
+//             options={options}
+//             series={series}
+//             type="bar"
+//             height={350}
+//           />
+//         </Box>
+//       </Card>
+//     </Box>
+//   )
+// }

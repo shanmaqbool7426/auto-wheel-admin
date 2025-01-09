cbefbdda8fd221a55bd361cdf1a7854584cb9805
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-10 01:12:51 +0500

    build issue

--- src/app/(root)/vehicle-add/page.jsx
+++ src/app/(root)/vehicle-add/page.jsx
@@ -1,27 +1,33 @@
 'use client';
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useGetVehicleByIdQuery } from '@/services/vehicle-manage';
 import AddVehicle from '@/modules/AddVehicle';
 import { LoadingOverlay } from '@mantine/core';
 
-export default function EditVehiclePage() {
+function AddVehicleWithParams() {
   const searchParams = useSearchParams();
   const id = searchParams.get('id');
-  console.log("id>>>>>>>>>", id)
-  // Only fetch if we have an ID
+  
   const { data: vehicleData, isLoading } = useGetVehicleByIdQuery(id, {
-    skip: !id // Skip the query if there's no ID
+    skip: !id
   });
 
   if (isLoading) {
     return <LoadingOverlay visible />;
   }
 
-  // If there's no ID, or no data needed, render empty form
   if (!id) {
     return <AddVehicle />;
   }
 
-  // If we have data, render form with edit data
   return <AddVehicle editData={vehicleData} />;
+}
+
+export default function EditVehiclePage() {
+  return (
+    <Suspense fallback={<LoadingOverlay visible />}>
+      <AddVehicleWithParams />
+    </Suspense>
+  );
 }

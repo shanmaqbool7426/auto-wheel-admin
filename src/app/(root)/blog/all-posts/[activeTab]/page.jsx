3805b268351b5d106a747345a39816f9dc51e9ea
shanmaqbool <shanmaqbool12345@gmail.com>
2025-01-15 14:33:04 +0500

    hi test  builf

--- src/app/(root)/blog/all-posts/[activeTab]/page.jsx
+++ src/app/(root)/blog/all-posts/[activeTab]/page.jsx
@@ -1,6 +1,6 @@
 import ClientWrapper from '@/components/ClientWrapper';
 import AllPosts from '@/modules/Blog/AllPosts';
-
+import { Suspense } from 'react';
 export default function AllPostsPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
